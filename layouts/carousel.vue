6a3d2d6681c0c7218f35ea5363407a067c54ce69
Bogdan Vasile <vasileeduardbogdan@gmail.com>
2026-07-16 10:23:29 +0300

    Update carousel.vue

--- layouts/carousel.vue
+++ layouts/carousel.vue
@@ -3,8 +3,9 @@ import { PAGE_SHELL_INSET_CLASS } from '~/utils/pageShell'
 </script>
 
 <template>
-  <div class="flex min-h-screen w-full flex-col bg-surface-page">
+  <div class="flex min-h-screen w-full min-w-0 flex-col bg-surface-page">
     <FavoritesFlash />
+    <AppNav />
     <main class="w-full min-w-0 flex-1">
       <div :class="PAGE_SHELL_INSET_CLASS">
         <slot />
